d52bda2b009eb6a0294b838d8ded8e41169432f1
Aashik <aashik1995@gmail.com>
2025-08-03 16:47:21 +0530

    encrypt password

--- src/main/java/com/webutil/urlshortener/Service/UserService.java
+++ src/main/java/com/webutil/urlshortener/Service/UserService.java
@@ -4,6 +4,8 @@ import com.webutil.urlshortener.Dto.UserDto;
 import com.webutil.urlshortener.Entity.User;
 import com.webutil.urlshortener.Repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +13,11 @@ public class UserService {
     @Autowired
     UserRepo userRepo;
 
+    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     public long addUser(UserDto userDto){
-        User newUser = userRepo.save(new User(userDto.getUsername(), userDto.getEmail(), userDto.getPassword()));
+        User newUser = userRepo.save(new User(userDto.getUsername(), userDto.getEmail(),
+                passwordEncoder.encode(userDto.getPassword())));
         return newUser.getUserId();
     }
 }
